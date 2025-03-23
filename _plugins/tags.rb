af3f396f121493c9cf98c4db5b935c37bfb20db7
POPKAMOKA <popkamoka@gmail.com>
2025-03-23 01:56:11 +0100

    feat: debug of tags generation

--- _plugins/tags.rb
+++ _plugins/tags.rb
@@ -3,7 +3,9 @@ module Jekyll
     safe true
 
     def generate(site)
+      puts "Génération des pages de tags..."  # Debug message
       tags = site.posts.docs.flat_map { |post| post.data['tags'] || [] }.to_set
+      puts "Tags trouvés : #{tags.to_a}"  # Affiche les tags trouvés
       tags.each do |tag|
         site.pages << TagPage.new(site, site.source, tag)
       end
@@ -12,6 +14,7 @@ module Jekyll
 
   class TagPage < Page
     def initialize(site, base, tag)
+      puts "Initialisation de la page pour le tag : #{tag}"  # Message pour chaque page tag
       @site = site
       @base = base
       @dir  = File.join('tag', tag)
